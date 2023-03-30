70249829890cd0844b1ead1dfce3764a1aa53d51
Pedro Gomes <70417307+pedrogones@users.noreply.github.com>
2023-03-30 00:32:21 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,44 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		//instanciando a classe
+		Lista l = new Lista();
+		
+		//inserido três dados na lista
+		l.insere(1,10);
+		l.insere(2,20);
+		l.insere(3,30);
+		
+		//printando na tela todos os elementos da lista
+		System.out.print("Lista: ");
+		for(int i = 1 ; i <= l.tamanho() ; i++) {	
+			System.out.print(l.elemento(i) + ", ");
+		}
+		
+		System.out.println();
+		
+		//verificando se a lista está vazia
+		System.out.println("A lista está vazia?  " + l.vazia());
+		
+		//Obtendo o tamanho da lista
+		System.out.println("Qual o tamanho da lista? " + l.tamanho());
+		
+		//modificando um elemento da lista
+		l.modificaElemento(25, 2);
+		System.out.print("Lista modificada: ");
+		for(int i = 1 ; i <= l.tamanho() ; i++) {	
+			System.out.print(l.elemento(i) + ", ");
+		}
+		
+		//Removendo um elemento da lista
+		l.remove(1);
+		System.out.print("\nLista com o elemento removido: ");
+		for(int i = 1 ; i <= l.tamanho() ; i++) {	
+			System.out.print(l.elemento(i) + ", ");
+		}
+		
+	}
+
+}
